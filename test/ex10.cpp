f5e66354b5e0e5473539680d783698b7f28e7130
Rosie <collard.florian@yahoo.com>
2024-06-22 20:28:55 +0200

    Ex10 prep

---
+++ test/ex10.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+//         .-.
+//   __   /   \   __
+//  (  `'.\   /.'`  )  matrix-cpp - ex10.cpp
+//   '-._.(;;;)._.-'
+//   .-'  ,`"`,  '-.
+//  (__.-'/   \'-.__)  By: Rosie (https://github.com/BlankRose)
+//      //\   /        Created at: June 22, 2024 [2:26 PM]
+//     ||  '-'
+/* ************************************************************************** */
+
+#include "common.hpp"
+
+// EXPLANATION SITE:
+// https://www.wikihow.com/Reduce-a-Matrix-to-Row-Echelon-Form
+
+int main()
+{
+    title("Row-Echelon Form");
+
+    results();
+}
